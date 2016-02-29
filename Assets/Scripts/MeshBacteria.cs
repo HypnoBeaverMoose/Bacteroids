8bca75e762334019debe89cef71eb40cd127e375
Yasen Atanasov <y.atanasoff@gmail.com>
2016-02-29 18:44:50 +0200

    added basic mesh deformation

--- Assets/Scripts/MeshBacteria.cs
+++ Assets/Scripts/MeshBacteria.cs
@@ -21,18 +21,20 @@ public class MeshBacteria : MonoBehaviour
 
 
     private List<Vector3> _vertices = null;
+    private List<Vector3> _vertexVelocities;
+    private List<Vector3> _originalVerticies;
     private List<float> _vertexCoDistance = null;
     private List<Vector3> _normals = new List<Vector3>();
     private List<Vector2> _points = new List<Vector2>();
     private List<int> _triangles = new List<int>();
-    private static Vector3[] _startVertices = new Vector3[] {   Vector3.zero, 
-                                                                new Vector3(-.5f, .5f, 0), 
-                                                                new Vector3(.5f, .5f, 0), 
-                                                                new Vector3(.5f, -.5f, 0), 
-                                                                new Vector3(-.5f, -.5f, 0) };   
+    private static Vector3[] _startVertices = new Vector3[] {   Vector3.zero,
+                                                                new Vector3(-.5f, .5f, 0),
+                                                                new Vector3(.5f, .5f, 0),
+                                                                new Vector3(.5f, -.5f, 0),
+                                                                new Vector3(-.5f, -.5f, 0) };
     private void Start()
     {
-        _vertices = new List<Vector3>(_startVertices);
+        Init();
         _collider = GetComponent<PolygonCollider2D>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _mesh = GetComponent<MeshFilter>().mesh;
@@ -40,8 +42,17 @@ public class MeshBacteria : MonoBehaviour
         Energy = _startEnergy;
         UpdateMesh();
     }
-
-    //private void OnCollisionEnter
+    
+    private void Init()
+    {
+        _vertices = new List<Vector3>(_startVertices);
+        _originalVerticies = new List<Vector3>(_startVertices);
+        _vertexVelocities = new List<Vector3>();
+        foreach (var vertex in _vertices)
+        {
+            _vertexVelocities.Add(Vector3.zero);
+        }
+    }
 
     private void UpdateMesh()
     {
@@ -69,17 +80,17 @@ public class MeshBacteria : MonoBehaviour
         _mesh.SetTriangles(_triangles, 0);
         _mesh.SetNormals(_normals);
         _mesh.SetUVs(0, _vertices);        
-        _mesh.UploadMeshData(false);
-        _collider.points = _points.ToArray();
+        //_mesh.UploadMeshData(false);
+        //_collider.points = _points.ToArray();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 force = (collision.relativeVelocity * collision.rigidbody.mass) / Time.fixedDeltaTime;
+        AddDeformingForce(collision.contacts[0].point, force);
         if (collision.gameObject.CompareTag("Projectile"))
-        {
-            Vector2 force = (collision.relativeVelocity * collision.rigidbody.mass) / Time.fixedDeltaTime;
-            Debug.Log(force.magnitude);
-            _rigidbody.AddForceAtPosition(-force, collision.contacts[0].point);
+        {            
+            _rigidbody.AddForceAtPosition(-force , collision.contacts[0].point);            
             var go = Instantiate<GameObject>(_energy);
             float damage = Random.Range(5, 20);
             this.Energy -= damage;
@@ -87,11 +98,50 @@ public class MeshBacteria : MonoBehaviour
             go.transform.position = collision.contacts[0].point;
             go.GetComponent<Rigidbody2D>().AddForceAtPosition(-force.magnitude * collision.contacts[0].normal, collision.contacts[0].point);
         }
+        
+
 
     }
-  
+
+    public void AddDeformingForce(Vector3 point, Vector3 force)
+    {
+        point = transform.InverseTransformPoint(point);
+        for (int i = 0; i < _vertices.Count; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point, Vector3 force)
+    {
+        Vector3 pointToVertex = _vertices[i] - point;
+        float attenuatedForce = force.magnitude / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.fixedDeltaTime;
+        _vertexVelocities[i] += force.normalized * velocity;
+       // _rigidbody.velocity -= (Vector2)((force.normalized * velocity) /(float)_vertexVelocities.Count);
+    }
+    void FixedUpdate()
+    {
+        float damping = 0.5f;
+        float springForce = 20;
+        Vector3 vel = Vector3.zero;
+        for (int i = 0; i < _vertices.Count; i++)
+        {
+            Vector3 velocity = _vertexVelocities[i];
+            vel += velocity;
+            Vector3 displacement = _vertices[i] - _originalVerticies[i];
+            velocity -= displacement * springForce * Time.fixedDeltaTime;
+            velocity *= 1f - damping * Time.fixedDeltaTime;
+            _vertexVelocities[i] = velocity;
+            _vertices[i] += velocity * Time.fixedDeltaTime;            
+        }
+
+        //_rigidbody.velocity = -(vel / _vertexVelocities.Count) * Time.deltaTime;
+        UpdateMesh();
+    }
     void Update()
     {
+
         if (Energy <= 0)
         {
             Destroy(gameObject);
