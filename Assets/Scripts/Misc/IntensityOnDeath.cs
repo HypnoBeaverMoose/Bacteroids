87f07f4a620f84e3e6a629a855e8ffe4bd070817
Yasen Atanasov <y.atanasoff@gmail.com>
2016-10-07 00:20:54 +0300

    adding more intensity spikes in the game

--- Assets/Scripts/Misc/IntensityOnDeath.cs
+++ Assets/Scripts/Misc/IntensityOnDeath.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class IntensityOnDeath : MonoBehaviour {
 
     [SerializeField]
-    private float _duration;
+    private float _colorDutation;
     [SerializeField]
-    private float _amount;
+    private float _colorIntentisy;
+    [SerializeField]
+    private float _deathDuration;
+    [SerializeField]
+    private float _deathIntentisy;
     [SerializeField]
     private UnityStandardAssets.ImageEffects.NoiseAndGrain _grain;
     // Use this for initialization
+    private bool running = false;
     void Start ()
     {
         Player.OnPlayerSpawned += OnPlayerSpawned;
@@ -18,23 +23,43 @@ public class IntensityOnDeath : MonoBehaviour {
     private void OnPlayerSpawned(Player obj)
     {
         obj.OnPlayerKilled += OnPlayerKilled;
+        obj.OnColorChanged += OnColorChanged;
+        if (!running)
+        {
+            StartCoroutine(SpikeIntensity(Color.white, _deathDuration, _deathIntentisy));
+        }
+    }
+
+    private void OnColorChanged(Color obj)
+    {
+        if (!running)
+        {
+            StartCoroutine(SpikeIntensity(obj, _colorDutation, _colorIntentisy));
+        }
     }
 
     private void OnPlayerKilled()
     {
-        StartCoroutine(SpikeIntensity());
+        if (!running)
+        {
+            StartCoroutine(SpikeIntensity(Color.white, _deathDuration, _deathIntentisy));
+        }
     }
 
-    private IEnumerator SpikeIntensity()
+    private IEnumerator SpikeIntensity(Color color, float duration, float intensity)
     {
+        running = true;
+        _grain.intensities = new Vector3(color.r, color.g, color.b);
         float velocity = 0;
         float originalValue = _grain.intensityMultiplier;
-        _grain.intensityMultiplier = _amount;
+        _grain.intensityMultiplier = intensity;
         while (Mathf.Abs(_grain.intensityMultiplier - originalValue) > 0.1f)
         {
-            _grain.intensityMultiplier = Mathf.SmoothDamp(_grain.intensityMultiplier, originalValue, ref velocity, _duration);
+            _grain.intensityMultiplier = Mathf.SmoothDamp(_grain.intensityMultiplier, originalValue, ref velocity, duration);
             yield return null;
         }
         _grain.intensityMultiplier = originalValue;
+        _grain.intensities = Vector3.one;
+        running = false;
     }  
 }
